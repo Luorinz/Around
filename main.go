1424f271f11a9877ca2d5bc10085370338b1e8ab
Anda Luo <luorinz@hotmail.com>
2019-04-30 14:06:45 -0700

    finish GCS

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"cloud.google.com/go/storage"
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/pborman/uuid"
 	elastic "gopkg.in/olivere/elastic.v3"
+	"io"
 	"log"
 	"net/http"
 	"reflect"
@@ -19,16 +22,19 @@ type Location struct {
 }
 
 type Post struct {
+	// helpful for json to parse User to user:username
 	User     string   `json:"user"`
 	Message  string   `json:"message"`
 	Location Location `json:"location"`
+	Url      string   `json:"url"`
 }
 
 const (
-	DISTANCE = "200km"
-	INDEX    = "around"
-	TYPE     = "post"
-	ES_URL   = "http://35.236.87.98:9200"
+	DISTANCE    = "200km"
+	INDEX       = "around"
+	TYPE        = "post"
+	ES_URL      = "http://35.235.114.72:9200"
+	BUCKET_NAME = "post-images-239021"
 )
 
 func main() {
@@ -82,12 +88,27 @@ func main() {
 //}
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Received one post request.")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+	// 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB (1MB = 1024 * 1024 bytes = 2^20 bytes)
+	// After you call ParseMultipartForm, the file will be saved in the server memory with maxMemory size.
+	// If the file size is larger than maxMemory, the rest of the data will be saved in a system temporary file.
 
-	decoder := json.NewDecoder(r.Body)
-	var p Post
-	if err := decoder.Decode(&p); err != nil {
-		panic(err)
+	r.ParseMultipartForm(32 << 20)
+
+	// parse form data
+	fmt.Printf("Received one post request %s\n", r.FormValue("message"))
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+
+	p := &Post{
+		User:    "1111",
+		Message: r.FormValue("message"),
+		Location: Location{
+			Lat: lat,
+			Lon: lon,
+		},
 	}
 
 	if containsFilterWords(&p.Message) {
@@ -98,14 +119,73 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Post received: %s\n", p.Message)
 
 	id := uuid.New()
+
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Image is not available", http.StatusInternalServerError)
+		fmt.Printf("Image is not available %v.\n", err)
+		return
+	}
+	defer file.Close()
+
+	ctx := context.Background()
+
+	// replace it with real bucket name
+	_, attrs, err := saveToGCS(ctx, file, BUCKET_NAME, id)
+	if err != nil {
+		http.Error(w, "GCS is not setup", http.StatusInternalServerError)
+		fmt.Printf("GCS is not setup %v\n", err)
+		return
+	}
+
+	// update the media link after saving to gcs
+	p.Url = attrs.MediaLink
+
 	// save to ES
-	saveToES(&p, id)
+	saveToES(p, id)
+}
+
+// Save an image to GCS
+func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+	// create a client
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer client.Close()
+
+	// create a bucket
+	bucket := client.Bucket(bucketName)
+
+	// check bucket
+	if _, err := bucket.Attrs(ctx); err != nil {
+		return nil, nil, err
+	}
+
+	obj := bucket.Object(name)
+	wc := obj.NewWriter(ctx)
+
+	if _, err = io.Copy(wc, r); err != nil {
+		return nil, nil, err
+	}
+	if err := wc.Close(); err != nil {
+		return nil, nil, err
+	}
+
+	if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return nil, nil, err
+	}
+
+	attrs, err := obj.Attrs(ctx)
+	fmt.Printf("Post is saved to GCS %s\n", attrs.MediaLink)
+	return obj, attrs, err
 }
 
 func saveToES(p *Post, id string) {
 	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
 	if err != nil {
 		panic(err)
+		return
 	}
 
 	_, err = es_client.Index().
@@ -116,7 +196,9 @@ func saveToES(p *Post, id string) {
 		Refresh(true).
 		Do()
 	if err != nil {
+		fmt.Printf("error: %v\n", err)
 		panic(err)
+		return
 	}
 
 	fmt.Printf("Post is saved to index: %s\n", p.Message)
@@ -170,7 +252,7 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 		p := item.(Post) // p = (Post) item
 		if !containsFilterWords(&p.Message) {
 			fmt.Printf("Post by %s: %s at lat %v and lon %v\n",
-			p.User, p.Message, p.Location.Lat, p.Location.Lon)
+				p.User, p.Message, p.Location.Lat, p.Location.Lon)
 			ps = append(ps, p)
 		}
 	}
